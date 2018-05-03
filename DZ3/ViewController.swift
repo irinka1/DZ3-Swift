574e250c944ebf504bb2f516c65d64060cd7caee
Ирина <irina@MacBook-Pro-Irina.local>
2018-05-03 14:43:56 +0300

    Initial Commit

---
+++ DZ3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DZ3
+//
+//  Created by Ирина on 03.05.18.
+//  Copyright © 2018 Ирина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
